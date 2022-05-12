eb0386e1273852896dc91e21e1a27dcabe3f783f
Ernesto <eojeda@gmx.es>
2022-05-12 14:33:09 -0400

    Creando un nuevo servicio para traducir archivos

--- src/main/java/com/example/demo/translate/TranslateFiles.java
+++ src/main/java/com/example/demo/translate/TranslateFiles.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.DataInput;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -21,45 +23,82 @@ import java.util.List;
 @RequestMapping("/")
 public class TranslateFiles {
 
-    @PostMapping(value = "translate", consumes = "multipart/form-data")
-    public ResponseEntity<String> translateFile(@RequestParam(value = "file", required = false) MultipartFile file){
-        //Procesando el fichero y creando el body del request
-        //TODO ver el formato del archivo para mapearlo al objeto
-        TranslatePostObject translateModel = new TranslatePostObject();
-        translateModel.setSrc("en");
-        translateModel.setTgt("es");
-        List<String> texts = new ArrayList<>();
-        texts.add("My dog is green");
-        texts.add("My dog is blue");
-        translateModel.setText(texts);
-        translateModel.setOrigin("PangeaMT");
-        translateModel.setUsername("admin@pangeanic.mt");
-        translateModel.setMode(2);
-        //Creando la llamaa al servicio
-        try{
-            RestTemplate restTemplate = new RestTemplate();
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            JSONObject req = new JSONObject();
-            String transUri = "http://prod.pangeamt.com:8080/NexRelay/v1/translate";
-            String result = "";
+    private final List<String> targetIdioms = Arrays.asList("es","zh","de","fr","ja","pt","ko","ru","it","pl","nl","tr","cs","ar","hi");
 
-            HttpEntity<TranslatePostObject> request = new HttpEntity<>(translateModel, headers);
-            ResponseEntity<String> responseEntity = restTemplate.exchange(transUri, HttpMethod.POST, request, String.class);
-            result = responseEntity.getBody();
-            //System.out.println(result);
-            List<List<LinkedHashMap<String, String>>> response = new ArrayList<>();
-            response = new ObjectMapper().readValue(result, List.class);
-            //System.out.println("response: " + response);
-            for (int i = 0; i < response.size(); i++){
-                List<LinkedHashMap<String, String>> trs = response.get(i);
-                LinkedHashMap<String, String> ss = trs.get(0);
-                System.out.println("Line" + (i+1) + "\t" + ss.get("src") + "\t" + ss.get("tgt"));
+    @PostMapping(value = "translate")
+    public ResponseEntity<String> translateFile(){
+        //Procesando el fichero y creando el body del request
+        int files = 0;
+        try {
+            File inputDir = new File("D:/4-Trabajo/translate/input");
+            files = inputDir.listFiles().length;
+            if (inputDir.isDirectory() && files > 0) {
+                for (int i = 0; i < files; i++) {
+                    File inputFile = inputDir.listFiles()[i];
+                    FileInputStream file = new FileInputStream(inputFile);
+                    BufferedReader br = new BufferedReader(new InputStreamReader(file, StandardCharsets.UTF_8));
+                    System.out.println("FILE: " + inputFile.getName());
+                    List<String> texts = new ArrayList<>();
+                    String line = br.readLine();
+                    while (line != null){
+                        texts.add(line);
+                        line = br.readLine();
+                    }
+                    br.close();
+                    for (String targetIdiom : targetIdioms) {
+                        int id = 1;
+                        //Creando el body del request de 20 en 20
+                        File fileoutput = new File("D:/4-Trabajo/translate/output/" + inputFile.getName() + "_en_" + targetIdiom + ".tsv");
+                        BufferedWriter bw = new BufferedWriter(new FileWriter(fileoutput));
+                        bw.write("id\tlocale\tinputText\ttranslation1\tmark\tnot_sure\tnote\n");
+                        for (int index = 0; index-1 < texts.size() / 20; index ++) {
+                            TranslatePostObject translateModel = new TranslatePostObject();
+                            translateModel.setSrc("en");
+                            translateModel.setTgt(targetIdiom);
+                            List<String> segments = (index < (texts.size() / 20))?texts.subList(index*20, index*20+20):texts.subList(index*20, texts.size());
+                            translateModel.setText(segments);
+                            translateModel.setOrigin("PangeaMT");
+                            translateModel.setUsername("admin@pangeanic.mt");
+                            translateModel.setMode(targetIdiom.equals("pt") ? 10 : 2);
+                            //Creando el archivo de salida
+
+                            //Creando la llamada al servicio
+                            try {
+                                RestTemplate restTemplate = new RestTemplate();
+                                HttpHeaders headers = new HttpHeaders();
+                                headers.setContentType(MediaType.APPLICATION_JSON);
+                                String transUri = "http://prod.pangeamt.com:8080/NexRelay/v1/translate";
+                                String result = "";
+
+                                HttpEntity<TranslatePostObject> request = new HttpEntity<>(translateModel, headers);
+                                ResponseEntity<String> responseEntity = restTemplate.exchange(transUri, HttpMethod.POST, request, String.class);
+                                result = responseEntity.getBody();
+                                //System.out.println(result);
+                                List<List<LinkedHashMap<String, String>>> response = new ArrayList<>();
+                                response = new ObjectMapper().readValue(result, List.class);
+                                //System.out.println("response: " + response);
+                                for (int x = 0; x < response.size(); x++) {
+                                    List<LinkedHashMap<String, String>> trs = response.get(x);
+                                    LinkedHashMap<String, String> ss = trs.get(0);
+                                    System.out.println("Line" + (x + 1) + "\t" + ss.get("src") + "\t" + ss.get("tgt"));
+                                    bw.write((id) + "\t" + translateModel.getSrc() + "\t" + ss.get("src") + "\t" + ss.get("tgt") + "\n");
+                                    id++;
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                return new ResponseEntity<>("FAILED", HttpStatus.INTERNAL_SERVER_ERROR);
+                            }
+                        }
+                        bw.close();
+                    }
+                }
+            } else{
+                return new ResponseEntity<>("FAILED", HttpStatus.BAD_REQUEST);
             }
-            return new ResponseEntity<>(result, HttpStatus.OK);
-        } catch (Exception e){
+        }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>("FAILED", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+        return new ResponseEntity<>("OK", HttpStatus.OK);
     }
 }
